787a9bc35b19cfe6cefa9e5a7fdf5e259c52ea44
mperdikeas <mperdikeas@gmail.com>
2020-03-17 17:22:52 +0200

    re-enabled login and improved login messages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,54 +18,54 @@ $(document).ready(doStuff);
 import GeometryContextProvider  from './context/geometry-context.jsx'
 
 function doStuff() {
-    if (false)
-        submitCredentials('admin', 'pass');
+  if (true)
+    submitCredentials('admin', 'pass');
 
   ReactDOM.render(
-          <GeometryContextProvider>
-            <App/>
-          </GeometryContextProvider>
+    <GeometryContextProvider>
+      <App/>
+    </GeometryContextProvider>
     , $('#app')[0]);
 }
 
 function submitCredentials(username, password) {
-    console.log('submitCredentials');
-    const url = 'https://127.0.0.1:8445/tree-cadaster-backend/jax-rs/main/login';
-    axios.post(url, {
-        username: username,
-        password: password
-    }).then(res => {
-        console.log('1');
-        if (res.data.err != null) {
-            console.log('2');
-            assert.fail(res.data.err);
-        } else {
-            console.log('3');
-            if (res.data.t.loginFailureReason===null) {
-                setCookie('access_token', res.data.t.accessToken, 0);
-            } else {
-                console.log('5');
-                assert.fail(res.data.t.loginFailureReason);
-            }
-        }
-    }).catch( err => {
-        console.log(err);
-        console.log(JSON.stringify(err));
-        console.log('6');
-        assert.fail(err);
-    });
+  console.log('submitCredentials');
+  const url = 'https://127.0.0.1:8445/tree-cadaster-backend/jax-rs/main/login';
+  axios.post(url, {
+    username: username,
+    password: password
+  }).then(res => {
+    if (res.data.err != null) {
+      console.log('login API call error');
+      assert.fail(res.data.err);
+    } else {
+      console.log('login API call success');
+      if (res.data.t.loginFailureReason===null) {
+        setCookie('access_token', res.data.t.accessToken, 0);
+        console.log('login was successful and cookie was set');
+      } else {
+        console.log('login was unsuccessful');
+        assert.fail(res.data.t.loginFailureReason);
+      }
+    }
+  }).catch( err => {
+    console.log(err);
+    console.log(JSON.stringify(err));
+    console.log('6');
+    assert.fail(err);
+  });
 }
 
 
 function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-    console.log(`Cookie set as ${document.cookie}`);
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+  console.log(`Cookie set as ${document.cookie}`);
 }
 
 
