62135dba2d3da927c3fc404be1deb0235339a9c2
mperdikeas <mperdikeas@gmail.com>
2020-03-26 19:54:56 +0200

    updated example backend API invocations script

--- rest-backend/example-api-invocations.sh
+++ rest-backend/example-api-invocations.sh
@@ -3,13 +3,13 @@
 if [ "$#" -ne 1 ]; then
     echo "The program expects a single argument: the value of the bearer authorizatio header"
     echo "Example: ./example-get-tree-invocation.sh eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImluc3RhbGxhdGlvbiI6ImExIiwiZXhwIjoxNTg1MjE0OTY4fQ.ND3-Un0LMzPhe_gpax9xcNa0InjxJL-hA13wnlUWlo8"
+    exit 1
 fi
 
 accessToken=$1
 
 echo $accessToken
 
-# to pass a cookie instead of an authorization header use: -b name=value
 curl -k https://127.0.0.1:8445/tree-cadaster-backend/jax-rs/main/getTrees -H "Authorization: Bearer ${accessToken}"
 
 echo
